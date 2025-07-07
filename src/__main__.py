c991030ebdb007bc62e273792b62db8d432b780c
laptop-coder <coderlaptop@gmail.com>
2025-07-07 17:28:35 +0300

    chore: add template of the main file

---
+++ src/__main__.py
@@ -0,0 +1,18 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+from config import BOT_TOKEN
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("Hello")
+
+
+def main() -> None:
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("hello", hello))
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
